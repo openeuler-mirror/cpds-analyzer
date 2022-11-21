791d542cfd6e1684a5c111067448f44a038f9ec3
张经纬 <dev16101@linx-info.com>
2022-11-21 15:13:25 +0800

    Add flag parse for cpds-analyzer

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -1,9 +1,13 @@
 package analyzer
 
-import "github.com/sirupsen/logrus"
+import (
+	"gitee.com/cpds/cpds-analyzer/config"
+	"github.com/sirupsen/logrus"
+)
 
-func RunAnalyzer() error {
+func RunAnalyzer(opts *config.Options) error {
 	logrus.Infof("Starting cpds-analyzer......")
+	logrus.Infof("Using config: database address: %s, database port: %s", opts.DatabaseAddress, opts.DatabasePort)
 	// TODO: complete this function
 	return nil
 }
