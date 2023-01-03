b2492c8dfd38a296657eb4fd3e86a3c3bc197a16
张经纬 <dev16101@linx-info.com>
2023-01-03 19:33:32 +0800

    Add utils: add get variance function

--- utils/calculation.go
+++ utils/calculation.go
@@ -2,6 +2,9 @@ package utils
 
 import (
 	"fmt"
+	"strconv"
+
+	"github.com/Knetic/govaluate"
 )
 
 func GetSum(nums ...float64) (sum float64) {
@@ -18,4 +21,37 @@ func GetMean(nums ...float64) (float64, error) {
 		return 0, fmt.Errorf("the divisor cannot be 0")
 	}
 	return sum / float64(n), nil
+}
+
+// Variance: s^2 = ((x1 - m)^2 + (x2 - m)^2 + ... + (xn - m)^2) / n
+func GetVariance(nums ...float64) (float64, error) {
+	e := fmt.Sprintf("(%s) / n", func(nums ...float64) (s string) {
+		for index := range nums {
+			if index == 0 {
+				s = fmt.Sprintf("(x%s - m)**2", strconv.Itoa(index+1))
+				continue
+			}
+			s = fmt.Sprintf("%s + (x%s - m)**2", s, strconv.Itoa(index+1))
+		}
+		return
+	}(nums...))
+	expr, _ := govaluate.NewEvaluableExpression(e)
+	parameters := make(map[string]interface{})
+
+	for index, value := range nums {
+		p := fmt.Sprintf("x%s", strconv.Itoa(index+1))
+		parameters[p] = value
+	}
+	m, err := GetMean(nums...)
+	if err != nil {
+		return 0, err
+	}
+	parameters["m"] = m
+	parameters["n"] = len(nums)
+
+	result, err := expr.Evaluate(parameters)
+	if err != nil {
+		return 0, err
+	}
+	return result.(float64), nil
 }
