9887ef502b9cf48db82d9faf5589943feafd165c
张经纬 <dev16101@linx-info.com>
2023-01-03 19:31:33 +0800

    Add utils: add get mean function

--- utils/calculation.go
+++ utils/calculation.go
@@ -1,8 +1,21 @@
 package utils
 
+import (
+	"fmt"
+)
+
 func GetSum(nums ...float64) (sum float64) {
 	for _, v := range nums {
 		sum += v
 	}
 	return
+}
+
+func GetMean(nums ...float64) (float64, error) {
+	sum := GetSum(nums...)
+	n := len(nums)
+	if n == 0 {
+		return 0, fmt.Errorf("the divisor cannot be 0")
+	}
+	return sum / float64(n), nil
 }
