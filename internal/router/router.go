c9314dbc0212d33b0525938ac220847f462201ed
SaarHV <dev16102@linx-info.com>
2023-08-30 17:51:19 +0800

    Initialize Database

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,12 +1,12 @@
-/* 
+/*
  *  Copyright 2023 CPDS Author
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *       https://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -69,6 +69,11 @@ func InitRouter(debug bool, config *config.Config, logger *zap.Logger, db *gorm.
 }
 
 func initDatabaseTable(db *gorm.DB) error {
+	result := db.Exec("CREATE DATABASE IF NOT EXISTS cpds").Error
+    if result != nil {
+        return result
+    }
+	db = db.Exec("USE cpds")
 	d := dbinitiator.New(db)
 	if err := d.Init(); err != nil {
 		return err
